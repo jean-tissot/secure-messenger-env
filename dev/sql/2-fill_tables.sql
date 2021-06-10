7c5199e669575a31bfeb300b898ba396f16c58d2
Jean Tissot <asphjt@gmail.com>
2021-06-10 22:47:24 +0200

    fix: remove all references to roles table

--- dev/sql/2-fill_tables.sql
+++ dev/sql/2-fill_tables.sql
@@ -10,12 +10,9 @@ INSERT INTO friends(user1_id, user2_id) VALUES
 INSERT INTO groups(name) VALUES
     ('Les rats'), ('');
 
-INSERT INTO roles(name) VALUES
-    ('administrateur'), ('utilisateur');
-
 INSERT INTO user_group(group_id, user_id, role_id) VALUES
-    ((SELECT id FROM groups WHERE name = 'Les rats'), (SELECT id FROM users WHERE pseudo = 'Paul'), (SELECT id FROM roles WHERE name = 'administrateur')),
-    ((SELECT id FROM groups WHERE name = 'Les rats'), (SELECT id FROM users WHERE pseudo = 'René'), (SELECT id FROM roles WHERE name = 'utilisateur')),
-    ((SELECT id FROM groups WHERE name = 'Les rats'), (SELECT id FROM users WHERE pseudo = 'Jules'), (SELECT id FROM roles WHERE name = 'utilisateur')),
-    ((SELECT id FROM groups WHERE name = ''), (SELECT id FROM users WHERE pseudo = 'René'), (SELECT id FROM roles WHERE name = 'administrateur')),
-    ((SELECT id FROM groups WHERE name = ''), (SELECT id FROM users WHERE pseudo = 'Yves-Albert'), (SELECT id FROM roles WHERE name = 'administrateur'));
+    ((SELECT id FROM groups WHERE name = 'Les rats'), (SELECT id FROM users WHERE pseudo = 'Paul'), 1),
+    ((SELECT id FROM groups WHERE name = 'Les rats'), (SELECT id FROM users WHERE pseudo = 'René'), 0),
+    ((SELECT id FROM groups WHERE name = 'Les rats'), (SELECT id FROM users WHERE pseudo = 'Jules'), 0),
+    ((SELECT id FROM groups WHERE name = ''), (SELECT id FROM users WHERE pseudo = 'René'), 1),
+    ((SELECT id FROM groups WHERE name = ''), (SELECT id FROM users WHERE pseudo = 'Yves-Albert'), 1);
